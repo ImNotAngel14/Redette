45d62471160e1921bbabf9682333ce39d346c9b7
unknown <barbosamane@hotmail.com>
2024-03-04 05:23:20 -0600

    Agrega documentación al codigo de la API

--- backend/index.js
+++ backend/index.js
@@ -4,29 +4,31 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-// Middleware para habilitar CORS
+// Activamos las dependencias necesarias para el proyecto.
 app.use(cors());
-
 app.use(express.json());
 
+// Indicamos escuchar a la API escuchar el puerto indicado.
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
 });
 
-// Ruta para el inicio de sesión
+// Ruta para el inicio de sesión.
 app.post('/login', (req, res) => 
 {
-  // Obtener los datos de inicio de sesión del cuerpo de la solicitud
+  
+  // Obtener los datos de inicio de sesión del cuerpo de la solicitud.
   const { username, password } = req.body;
   
-  //Creamos la conexion a mysql
+  //Creamos la conexion a mysql.
   const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'Redette'
   });
-  // Llamamos al procedure de autenticacion del login
+
+  // Llamamos al procedure de autenticacion del login.
   try {
     connection.connect();
     connection.query('CALL Redette.sp_authLogin("'+username+'", "'+password+'");', (error, results, fields) => {
@@ -37,24 +39,26 @@ app.post('/login', (req, res) =>
       }
       else
       {
+
+        // Respondemos el resultado de la autenticación del usuario.
         res.json({ auth: results[0][0].found_user });
       }
     });
     
   } catch (error) {
 
-    // Imprimimos en consola el error capturado y respondemos al front end
+    // Imprimimos en consola el error capturado y respondemos al front end.
     console.error('Error al conectar a la base de datos:', error);
 
     res.json({ auth: 0 });
 
-    // Aqui deberia enviar la respuesta como un codigo http
+    // Aqui deberia enviar la respuesta como un codigo http.
     // ...
   }
   finally
   {
 
-    // Finalizamos conexión a la base de datos
+    // Finalizamos conexión a la base de datos.
     connection.end();
   }
 });
