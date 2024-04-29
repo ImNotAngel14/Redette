c9d7503a67458c9892d373c75bafd6c984e3801b
unknown <barbosamane@hotmail.com>
2024-04-29 16:31:49 -0600

    Implementa prisma

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 const app = express();
 const port = 3000;
 
@@ -8,57 +11,33 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  console.log('Desconectado de la base de datos.');
+  process.exit();
+});
+
 // Indicamos escuchar a la API escuchar el puerto indicado.
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
 });
 
-// Ruta para el inicio de sesión.
-app.post('/login', (req, res) => 
-{
-  
+app.post('/login', async (req, res) => {
   // Obtener los datos de inicio de sesión del cuerpo de la solicitud.
   const { username, password } = req.body;
-  
-  //Creamos la conexion a mysql.
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'Redette'
-  });
-
-  // Llamamos al procedure de autenticacion del login.
   try {
-    connection.connect();
-    connection.query('CALL Redette.sp_authLogin("'+username+'", "'+password+'");', (error, results, fields) => {
-      if (error)
-      {
-        console.error('Error al ejecutar la consulta:', error);
-        res.json({ auth: 0 });
-      }
-      else
-      {
-
-        // Respondemos el resultado de la autenticación del usuario.
-        res.json({ auth: results[0][0].found_user });
+    // Buscar el usuario por nombre de usuario
+    const user = await prisma.usuario.findMany({
+      where: {
+        usuario: username,
+        contrasena: password
       }
     });
-    
+
+    console.log(user); // Imprimir el usuario encontrado en la consola
+    res.json(user); // Devolver el usuario como respuesta al cliente
   } catch (error) {
-
-    // Imprimimos en consola el error capturado y respondemos al front end.
-    console.error('Error al conectar a la base de datos:', error);
-
-    res.json({ auth: 0 });
-
-    // Aqui deberia enviar la respuesta como un codigo http.
-    // ...
-  }
-  finally
-  {
-
-    // Finalizamos conexión a la base de datos.
-    connection.end();
+    console.error('Error al obtener usuario:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
